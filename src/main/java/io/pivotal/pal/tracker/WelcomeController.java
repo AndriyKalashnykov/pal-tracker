5f8ad2c27981b326fb01a3159476924ecb45dfb9
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2019-06-10 21:55:39 -0400

    LAB 2 - Configuring an App

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,35 @@
 package io.pivotal.pal.tracker;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+   private @Value("${welcome.message}") String str;
+
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(final String val) {
+        this.str = val;
+    }
+
+    public WelcomeController() {
+        this.setStr(str);
+    }
+
+
+    public WelcomeController(String message) {
+        this.setStr(message);
+    }
+
     @GetMapping("/")
-    public String sayHellp(){
-        return "Hello";
+    public String sayHello(){
+        return this.getStr();
     }
 }
