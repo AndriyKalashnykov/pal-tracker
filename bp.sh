75378ebc53229c6e0f60a7dd0365dbb8810d173c
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2019-06-13 01:14:02 -0400

    added to build push script

--- bp.sh
+++ bp.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # set -xv
 
+
 gradle clean build
+
+cf delete pal-tracker -r -f
 cf push pal-tracker --random-route --no-start -p ./build/libs/pal-tracker.jar
 cf restart pal-tracker
+
+cf create-service p.mysql db-small tracker-database
+cf service tracker-database
+
+cf bind-service pal-tracker tracker-database
